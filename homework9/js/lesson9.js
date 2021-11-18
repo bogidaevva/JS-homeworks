b1f2540c2eceaaf05a1490989a8cda60152d3796
bogidaeva <o.bogidaevva@gmail.com>
2021-11-18 13:50:45 +0300

    Домашняя работа 9: добавлен №2 и задание с книгами

--- homework9/js/lesson9.js
+++ homework9/js/lesson9.js
@@ -31,45 +31,6 @@ let animals = [
     }
 ];
 
-/*  <section>
-        <div class = "card">
-            <h3>Люся</h3>
-            <span class = "age">1 год</span>
-            <span class = "color">трехцветная</span>
-            <div class = "additional-info">
-                <span class = "vaccinations">вакцинация есть/нет</span>
-                <span class = "passport">паспорт есть/нет</span>
-            </div>
-        </div>
-        <div class = "card">
-            <h3>Том</h3>
-            <span class = "age">3 месяца</span>
-            <span class = "color">белый</span>
-            <div class = "additional-info">
-                <span class = "vaccinations">вакцинация есть/нет</span>
-                <span class = "passport">паспорт есть/нет</span>
-            </div>
-        </div>
-        <div class = "card">
-            <h3>Макс</h3>
-            <span class = "age">2</span>
-            <span class = "color">серый</span>
-            <div class = "additional-info">
-                <span class = "vaccinations">вакцинация есть/нет</span>
-                <span class = "passport">паспорт есть/нет</span>
-            </div>
-        </div>
-        <div class = "card">
-            <h3>Василий</h3>
-            <span class = "age">3</span>
-            <span class = "color">черный</span>
-            <div class = "additional-info">
-                <span class = "vaccinations">вакцинация есть/нет</span>
-                <span class = "passport">паспорт есть/нет</span>
-            </div>
-        </div>
-    </section> */
-
 let petsSection = document.createElement("section");
 
 for (let obj of animals) {
@@ -80,25 +41,21 @@ for (let obj of animals) {
     name.innerText = obj.name;
 
     let age = document.createElement("span");
-    age.classList.add("age");
     age.innerText = obj.age;
 
     let color = document.createElement("span");
-    color.classList.add("color");
     color.innerText = obj.color;
 
     let additInfo = document.createElement("div");
     additInfo.classList.add("additional-info");
 
     let vaccine = document.createElement("span");
-    vaccine.classList.add("vaccinations");
     Object.values(obj).forEach((prop) => {
         if (prop.vaccinations === true) vaccine.innerText = "вакцинация есть";
         else vaccine.innerText = "вакцинации нет";
     });
 
     let pass = document.createElement("span");
-    pass.classList.add("passport");
     Object.values(obj).forEach((prop) => {
         if (prop.passport === true) pass.innerText = "паспорт есть";
         else pass.innerText = "паспорта нет";
@@ -168,20 +125,136 @@ let goods = [
     }
 ];
 
-// function generateTable(arr){
-//     let table = document.createElement("table");
-//     //[{} {} {} {}]
-    
-//     for (let obj of arr) {
-//         // {
-//         //     title: "Piano",
-//         //     price: 3000,
-//         //     count: 25,
-//         // }
-    
+function generateTable(arr){
+    let table = document.createElement("table");
+    table.style.backgroundColor = "#7E60A1";
+    //создаем первую строку таблицы с названиями столбцов
+    let row = table.insertRow(0);
+    row.style.cssText = "background-color: #C999FF; text-align: center";
+    for (let key in arr[0]) {
+        let cell = row.insertCell();
+        cell.innerText = key.toLocaleUpperCase();
+    }
 
-//     document.body.append(table);
-    
-// }
+    for (let obj of arr){
+        let row = table.insertRow();
+        for (let key in obj){
+           let cell = row.insertCell();
+           cell.innerText = obj[key];
+           cell.style.cssText = "background-color: #fff; text-align: center; padding-top: 8px; padding-bottom: 8px";
+        }
+    }
+    document.body.append(table);
+}
 
-// console.log(generateTable(articles));
+generateTable(goods);
+generateTable(articles);
+
+// TODO 
+// books-section: отобразить информацию о книгах
+/*
+для каждого жанра отдельный div:
+<div>
+    <h2></h2> название жанра
+    <div class="books"> все книги одного жанра
+        <article> книга
+            <span></span> название книги
+            <img> обложка
+            <p></p> описание
+            <a>Читать</a>
+        </article>
+    </div>
+</div>
+*/
+
+let booksByGenre = [  
+    {
+        genreName: "Crafts",
+        books: [
+            {
+                id: 8,
+                title: "Lorem ipsum",
+                description: "A ab aliquam consequatur ea fugit inventore laudantium natus, necessitatibus non nulla " +
+                    "optio quae quod quos repudiandae rerum, tempore",
+                img: "https://picsum.photos/1000/1000?random=20"
+            },
+            {
+                id: 10,
+                title: "Aliquam consequatur",
+                description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab aliquam consequatur " +
+                    "ea fugit inventore laudantium natus",
+                img: "https://picsum.photos/1000/1000?random=21"
+            },
+            {
+                id: 34,
+                title: "Laudantium",
+                description: "A ab aliquam consequatur ea fugit inventore laudantium natus, necessitatibus non " +
+                    "nulla optio quae quod quos repudiandae rerum, tempore, voluptate. laudantium natus, necessitatibus " +
+                    "non nulla optio quae quod quos repudiandae rerum, tempore, voluptate.",
+                img: "https://picsum.photos/1000/1000?random=22"
+            }
+        ]
+    },
+    {
+        genreName: "Thriller",
+        books: [
+            {
+                id: 4,
+                title: "Necessitatibus",
+                description: "A ab aliquam consequatur ea fugit inventore laudantium natus, necessitatibus non" +
+                    " nulla optio quae quod quos repudiandae rerum, tempore",
+                img: "https://picsum.photos/1000/1000?random=23"
+            },
+            {
+                id: 13,
+                title: "Nulla optio quae quod",
+                description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab aliquam consequatur ea " +
+                    "fugit inventore laudantium natus",
+                img: "https://picsum.photos/1000/1000?random=24"
+            },
+            {
+                id: 59,
+                title: "Non nulla",
+                description: "A ab aliquam consequatur ea fugit inventore laudantium natus, necessitatibus non " +
+                    "nulla optio quae quod quos repudiandae rerum, tempore, voluptate. laudantium natus," +
+                    " necessitatibus non nulla optio quae quod quos repudiandae rerum, tempore, voluptate.",
+                img: "https://picsum.photos/1000/1000?random=25"
+            }
+        ]
+    }
+];
+
+let bookSection = document.getElementsByClassName("books-section")[0];
+
+function addBooks(booksArr, element) { //в element все это добавляем class = "books-section"
+    //1 перебираем массив booksArr = [{Crafts [книги {}{}{}]} {Thriller [книги{}{}{}]}]:
+    for (let bookGenre of booksArr){ // в bookGenre копируется название {Crafts [книги {}{}{}]}
+        // bookGenre = [книги {}{}{}]
+        //1.1 создаем большой внешний див
+        let genreDiv = document.createElement("div");
+        genreDiv.classList.add("genre");
+        //1.2 создаем заголовок h2
+        let genreTitle = document.createElement("h2");
+        genreTitle.innerText = bookGenre.genreName;
+        //1.3 создаем div class="books"
+        let booksDiv = document.createElement("div");
+        booksDiv.classList.add("books");
+        //1.4 перебор booksArr.books
+        for (let book of bookGenre.books){
+            //создаем article (все содержимое article через innerHTML)
+            let bookArticle = document.createElement("article");
+            bookArticle.innerHTML = `
+                <span>${book.title}</span>
+                <img src="${book.img}" alt="${book.title}">
+                <p>${book.description}</p> 
+                <a>Читать</a>
+            `
+            //на каждой итерации в див с книгами вкладываем созданную article с конкретной книгой 
+            booksDiv.append(bookArticle);
+        }
+        //вкладываем див с книгами жанра и заголовок в большой див 
+        genreDiv.append(genreTitle, booksDiv);
+        element.append(genreDiv);
+    }
+}
+addBooks(booksByGenre, bookSection);
